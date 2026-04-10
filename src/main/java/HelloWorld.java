12a5fabd880e01d49e3a4389c4b592f8a0e657bb
revanthundavalli07-byte <revanthundavalli07@gmail.com>
2026-04-10 09:00:09 +0530

    uc5

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,12 +1,14 @@
-
 public class HelloWorld {
     public static void main(String[] args) {
-        if (args.length > 0) {
-            System.out.println("Hello " + args[0]);
+
+        // If no arguments, print Hello World
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
         } else {
-            System.out.println("Hello User");
+            // Loop through all names
+            for (int i = 0; i < args.length; i++) {
+                System.out.println("Hello, " + args[i] + "!");
+            }
         }
     }
 }
-
-
